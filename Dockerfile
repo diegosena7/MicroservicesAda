bd6fd228cd83f905e717a49a766274ee22f7d9c3
Luiz Cantrella <luizcantrella@gmail.com>
2023-03-24 22:01:02 -0300

    configurar subinda do sql

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM openjdk:17-alpine
-
-WORKDIR /app
-
-COPY configuration/target/*.jar app.jar
-
-EXPOSE 8081
-
-ENTRYPOINT ["java", "-jar", "app.jar"]
--- run-mutation.cmd
+++
@@ -1,6 +0,0 @@
-ECHO.
-FOR /f %%i IN ('dir target /s /b') DO RD /s /q %%i
-CLS
-
-ECHO.
-CALL mvn -e clean install org.pitest:pitest-maven:mutationCoverage
