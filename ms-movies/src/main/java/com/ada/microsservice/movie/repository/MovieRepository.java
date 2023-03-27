6afd0117eb917e5705a35c3d45679f1f41268f90
diegosena7 <diego.tjdosantos@gmail.com>
2023-03-27 20:34:46 -0300

    add config JPA in repository

--- ms-movies/src/main/java/com/ada/microsservice/movie/repository/MovieRepository.java
+++ ms-movies/src/main/java/com/ada/microsservice/movie/repository/MovieRepository.java
@@ -3,12 +3,13 @@ package com.ada.microsservice.movie.repository;
 import com.ada.microsservice.movie.model.MovieEntity;
 import com.cosium.spring.data.jpa.entity.graph.repository.EntityGraphJpaRepository;
 import com.cosium.spring.data.jpa.entity.graph.repository.EntityGraphQuerydslPredicateExecutor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MovieRepository extends EntityGraphJpaRepository<MovieEntity, Long>, EntityGraphQuerydslPredicateExecutor<MovieEntity> {
+public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
 
     Optional<MovieEntity> findByImdbIdIgnoreCase(final String ImdbId);
 }
