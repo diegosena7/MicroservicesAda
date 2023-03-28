557c0a23b35ffbb570545413fe60418bbf49bfbe
Michelle <michelledantasluiz@gmail.com>
2023-03-27 21:21:17 -0300

    Adicionando endpoint imdb

--- ms-movies/src/main/java/com/ada/microsservice/movie/controller/MovieController.java
+++ ms-movies/src/main/java/com/ada/microsservice/movie/controller/MovieController.java
@@ -1,10 +1,14 @@
 package com.ada.microsservice.movie.controller;
 
+import com.ada.microsservice.movie.model.Movie;
 import com.ada.microsservice.movie.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "movies")
@@ -16,6 +20,11 @@ public class MovieController {
     @GetMapping
     public Long countAll() {
         return movieService.countAll();
+    }
+
+    @GetMapping("/{imdb}")
+    public Optional<Movie> execute(@PathVariable String imdb){
+        return movieService.execute(imdb);
     }
 
 }
--- ms-movies/src/main/java/com/ada/microsservice/movie/service/MovieService.java
+++ ms-movies/src/main/java/com/ada/microsservice/movie/service/MovieService.java
@@ -1,16 +1,27 @@
 package com.ada.microsservice.movie.service;
 
+import com.ada.microsservice.movie.model.Movie;
+import com.ada.microsservice.movie.model.MovieEntity;
+import com.ada.microsservice.movie.model.mapper.MovieMapper;
 import com.ada.microsservice.movie.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository movieRepository;
-
+    private final MovieMapper movieMapper = Mappers.getMapper(MovieMapper.class);
     public Long countAll() {
         return movieRepository.count();
+    }
+
+    public Optional<Movie> execute (String imdb){
+        final Optional<MovieEntity> optional = movieRepository.findByImdbIdIgnoreCase(imdb);
+        return optional.map(movieMapper::of);
     }
 }
